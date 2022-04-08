93bb442da543ac3dee37d524ed94341e18578946
Khoa <khoa.nguyendang@outlook.com>
2022-04-09 01:07:24 +0700

    adjustment

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import reportWebVitals from './reportWebVitals';
 
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
-    <Provider store={store}>
-        <I18nextProvider i18n={i18n}>
-            <App />
-        </I18nextProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
